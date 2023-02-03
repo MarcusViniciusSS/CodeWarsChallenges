a43e85a77d225d0f21069a3516784814712a2207
Marcus.Silva <marcus.silva@xpi.com.br>
2023-02-03 13:59:32 -0300

    feat(codewars): resolve problem MultiplyWordInString

---
+++ CodeWarsChallenges/CodeWarsChallenges/Strings/7kyu/Multiply-Word-In-String/MultiplyWordInString.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace CodeWarsChallenges.Strings._7kyu.Multiply_Word_In_String;
+
+public static class MultiplyWordInString
+{
+    public static string ModifyMultiply(string str,int loc,int num)
+    {
+        var strs = str.Split(" ");
+
+        if (strs[loc] == null) return string.Empty;
+
+        var stringbuilder = new StringBuilder((strs[loc].Length * num) + num);
+
+        for (var i = 0; i < num; i++)
+        {
+            stringbuilder.Append(strs[loc]);
+
+            if ((i + 1) == num)
+            {
+                continue;
+            }
+            
+            stringbuilder.Append('-');
+        }
+        
+        return stringbuilder.ToString();
+    }
+}
