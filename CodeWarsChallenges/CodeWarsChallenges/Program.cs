0617ac8273611cbe78a7c79de3b3ff7132ef1896
Marcus.Silva <marcus.silva@xpi.com.br>
2023-02-03 13:59:05 -0300

    feat(console): create solution and project.

---
+++ CodeWarsChallenges/CodeWarsChallenges/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AlgTester.API;
+using CodeWarsChallenges.Strings._7kyu.Multiply_Word_In_String;
+
+{
+    Console.Write("7kyu  --- Strings -----");
+    var s = MultiplyWordInString.ModifyMultiply;
+    SolutionTester.New().WithSolution(s).Run();
+}
