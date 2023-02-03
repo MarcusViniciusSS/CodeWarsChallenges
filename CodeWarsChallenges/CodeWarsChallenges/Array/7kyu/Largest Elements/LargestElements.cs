9fcdfd82ecbbd1ba6b21f093d44f5f9af4fffa0d
Marcus.Silva <marcus.silva@xpi.com.br>
2023-02-03 14:48:43 -0300

    feat(codewars): Resolve Largest Elements

---
+++ CodeWarsChallenges/CodeWarsChallenges/Array/7kyu/Largest Elements/LargestElements.cs
@@ -0,0 +1,20 @@
+﻿namespace CodeWarsChallenges.Array._7kyu.Largest_Elements;
+
+public static class LargestElements
+{
+    public static List<int> Largest(int n, List<int> xs)
+    {
+        if (n == 0) return new();
+
+        var list = xs.OrderByDescending(item => item).ToList();
+        var result = new Stack<int>(n);
+
+        for (var i = 0; i < n; i++)
+        {
+            result.Push(list[i]);
+        }
+        
+        // Find the n highest elements in a list
+        return result.ToList();
+    }
+}
---
+++ CodeWarsChallenges/CodeWarsChallenges/Array/7kyu/Largest Elements/LargestElements_Question.md
@@ -0,0 +1,11 @@
+﻿Write a program that outputs the top n elements from a list.
+
+Example:
+
+```csharp
+    Kata.Largest(2, new List<int> {7, 6, 5, 4, 3, 2, 1}) => new List<int> {6, 7}
+```
+
+``
+ARRAYS FUNDAMENTALS
+``
