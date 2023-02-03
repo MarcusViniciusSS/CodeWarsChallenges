3ccf17aa04752419e5ecc7bc879f13f543614a3d
Marcus.Silva <marcus.silva@xpi.com.br>
2023-02-03 14:49:13 -0300

    feat(codewars): add suite tests.

--- CodeWarsChallenges/CodeWarsChallenges/Program.cs
+++ CodeWarsChallenges/CodeWarsChallenges/Program.cs
@@ -1,9 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using AlgTester.API;
+using CodeWarsChallenges.Array._7kyu.Largest_Elements;
 using CodeWarsChallenges.Strings._7kyu.Multiply_Word_In_String;
 
 {
     Console.Write("7kyu  --- Strings -----");
-    var s = MultiplyWordInString.ModifyMultiply;
-    SolutionTester.New().WithSolution(s).Run();
+    var sMultiply = MultiplyWordInString.ModifyMultiply;
+    SolutionTester.New().WithSolution(sMultiply).Run();
+    
+    
+    Console.Write("7kyu  --- Arrays -----");
+    
+    var sLargest = LargestElements.Largest;
+    SolutionTester.New().WithSolution(sLargest).Run();
 }
