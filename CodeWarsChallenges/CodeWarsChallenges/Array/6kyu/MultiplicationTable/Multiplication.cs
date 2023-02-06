96c7f0e1f4d4b72207f1af84663407544044a78e
Marcus.Silva <marcus.silva@xpi.com.br>
2023-02-06 10:47:40 -0300

    feat(codewars): Multiplaction Table

---
+++ CodeWarsChallenges/CodeWarsChallenges/Array/6kyu/MultiplicationTable/Multiplication.cs
@@ -0,0 +1,19 @@
+﻿namespace CodeWarsChallenges.Array._6kyu.MultiplicationTable;
+
+public static class Multiplication
+{
+    public static int[,] MultiplicationTable(int size)
+    {
+        var table = new int [size, size];
+
+        for (var line = 1; line <= size; line++)
+        {
+            for (var column = 0; column < size; column++)
+            {
+                table[line - 1, column] = line * (1 + column);
+            }
+        }
+
+        return table;
+    }
+}
