0320aa97ae75bb15ad8c68ae0f374deed9318832
Marcus.Silva <marcus.silva@xpi.com.br>
2023-02-06 10:47:59 -0300

    feat(startup): add new solution.

--- CodeWarsChallenges/CodeWarsChallenges/Program.cs
+++ CodeWarsChallenges/CodeWarsChallenges/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using AlgTester.API;
+using CodeWarsChallenges.Array._6kyu.MultiplicationTable;
 using CodeWarsChallenges.Array._7kyu.Largest_Elements;
 using CodeWarsChallenges.Strings._6kyu.Highest_Scoring_Word;
 using CodeWarsChallenges.Strings._7kyu.Multiply_Word_In_String;
@@ -20,4 +21,10 @@ using CodeWarsChallenges.Strings._7kyu.Multiply_Word_In_String;
     Console.Write("6kyu  --- Strings -----");
     var sHighest = HighestScoringWord.High;
     SolutionTester.New().WithSolution(sHighest).Run();
+}
+
+{
+    Console.Write("6kyu  --- Arrays -----");
+    var sMultiplationTable = Multiplication.MultiplicationTable;
+    SolutionTester.New().WithSolution(sMultiplationTable).Run();
 }
