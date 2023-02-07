8bdc06004b7278dd12db1db1ae21c5051685a94b
Marcus.Silva <marcus.silva@xpi.com.br>
2023-02-07 15:21:15 -0300

    feat(MexicanWave): Add on Program.cs

--- CodeWarsChallenges/CodeWarsChallenges/Program.cs
+++ CodeWarsChallenges/CodeWarsChallenges/Program.cs
@@ -3,28 +3,32 @@ using AlgTester.API;
 using CodeWarsChallenges.Array._6kyu.MultiplicationTable;
 using CodeWarsChallenges.Array._7kyu.Largest_Elements;
 using CodeWarsChallenges.Strings._6kyu.Highest_Scoring_Word;
+using CodeWarsChallenges.Strings._6kyu.Mexican_Wave;
 using CodeWarsChallenges.Strings._7kyu.Multiply_Word_In_String;
 
 {
-    Console.Write("7kyu  --- Strings -----");
+    Console.WriteLine("7kyu  --- Strings -----");
     var sMultiply = MultiplyWordInString.ModifyMultiply;
     SolutionTester.New().WithSolution(sMultiply).Run();
     
     
-    Console.Write("7kyu  --- Arrays -----");
+    Console.WriteLine("7kyu  --- Arrays -----");
     
     var sLargest = LargestElements.Largest;
     SolutionTester.New().WithSolution(sLargest).Run();
 }
 
 {
-    Console.Write("6kyu  --- Strings -----");
+    Console.WriteLine("6kyu  --- Strings -----");
     var sHighest = HighestScoringWord.High;
     SolutionTester.New().WithSolution(sHighest).Run();
+    
+    var sWave = MexicanWave.Wave;
+    SolutionTester.New().WithSolution(sWave).Run();
 }
 
 {
-    Console.Write("6kyu  --- Arrays -----");
+    Console.WriteLine("6kyu  --- Arrays -----");
     var sMultiplationTable = Multiplication.MultiplicationTable;
     SolutionTester.New().WithSolution(sMultiplationTable).Run();
 }
