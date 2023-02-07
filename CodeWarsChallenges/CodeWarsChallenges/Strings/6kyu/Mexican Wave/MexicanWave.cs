49c36df5dfebd316acbf052eee1c1fd046757848
Marcus.Silva <marcus.silva@xpi.com.br>
2023-02-07 15:20:46 -0300

    feat(MexicanWave): Solution

---
+++ CodeWarsChallenges/CodeWarsChallenges/Strings/6kyu/Mexican Wave/MexicanWave.cs
@@ -0,0 +1,20 @@
+﻿namespace CodeWarsChallenges.Strings._6kyu.Mexican_Wave;
+
+public static class MexicanWave
+{
+    public static List<string> Wave(string str)
+    {
+        var result = new List<string>(str.Length);
+        if (str.Length <= 0) return result;
+        
+        for (var i = 0; i < str.Length; i++)
+        {
+            if (str[i] == ' ') continue;
+            var temp = str.ToCharArray();
+            temp[i] = char.ToUpper(temp[i]);
+            result.Add(new (temp));
+        }
+        
+        return result;
+    }
+}
