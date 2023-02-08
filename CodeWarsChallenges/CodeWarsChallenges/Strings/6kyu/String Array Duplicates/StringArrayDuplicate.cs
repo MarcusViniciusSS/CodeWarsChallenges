b1b5b3d46a722004d7550a34c7cba4f87e530615
Marcus.Silva <marcus.silva@xpi.com.br>
2023-02-08 12:31:23 -0300

    feat(solution): StringArrayDuplicate

---
+++ CodeWarsChallenges/CodeWarsChallenges/Strings/6kyu/String Array Duplicates/StringArrayDuplicate.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace CodeWarsChallenges.Strings._6kyu.String_Array_Duplicates;
+
+public static class StringArrayDuplicate
+{
+    public static string[] dup(string[] arr)
+    {
+        var strings = new string[arr.Length];
+        
+        for (var i = 0; i < arr.Length; i++)
+        {
+            var n = 0;
+            var stringBuilder = new StringBuilder(arr[i].Length);
+            var currentLetter = ' ';
+            while (n < arr[i].Length)
+            {
+                if (char.IsWhiteSpace(currentLetter))
+                {
+                    stringBuilder.Append(arr[i][n]);
+                    currentLetter = arr[i][n];
+                } 
+                else if (!currentLetter.Equals(arr[i][n]))
+                {
+                    stringBuilder.Append(arr[i][n]);
+                    currentLetter = arr[i][n];
+                }
+
+                n++;
+            }
+            
+            strings[i] = stringBuilder.ToString();
+        }
+        
+        return strings;
+    }
+}
