70d18d28ce989908962fc684324750f235d73469
Marcus.Silva <marcus.silva@xpi.com.br>
2023-02-09 11:20:57 -0300

    feat(SumOfParts): implementation

---
+++ CodeWarsChallenges/CodeWarsChallenges/Algorithms/6kyu/SumOfParts/SumOfParts.cs
@@ -0,0 +1,16 @@
+﻿namespace CodeWarsChallenges.Algorithms._6kyu.SumOfParts;
+
+public static class SumOfParts
+{
+    public static int[] PartsSums(int[] ls)
+    {
+        var sumParts = new int[ls.Length + 1];
+
+        for (var i = sumParts.Length-2; i >= 0; i--)
+        {
+            sumParts[i] = ls[i] + sumParts[i + 1];
+        }
+        
+        return sumParts;
+    }
+}
