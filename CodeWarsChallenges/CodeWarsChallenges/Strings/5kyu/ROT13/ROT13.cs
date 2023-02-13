a8789eaae624fd73c35b1534a3bc46fb5082f7e2
Marcus.Silva <marcus.silva@xpi.com.br>
2023-02-13 12:53:39 -0300

    feat(rot13): add solution.

---
+++ CodeWarsChallenges/CodeWarsChallenges/Strings/5kyu/ROT13/ROT13.cs
@@ -0,0 +1,20 @@
+﻿namespace CodeWarsChallenges.Strings._5kyu.ROT13;
+
+public static class ROT13
+{
+    public static string Encrypt(string input)
+    {
+        var inputArray = input.ToCharArray();
+        for (var i = 0; i < inputArray.Length; i++)
+        {
+            var c = inputArray[i];
+            inputArray[i] = c switch
+            {
+                >= 'a' and <= 'z' => (char) ('a' + (c - 'a' + 13) % 26),
+                >= 'A' and <= 'Z' => (char) ('A' + (c - 'A' + 13) % 26),
+                _ => inputArray[i]
+            };
+        }
+        return new string(inputArray);
+    }
+}
