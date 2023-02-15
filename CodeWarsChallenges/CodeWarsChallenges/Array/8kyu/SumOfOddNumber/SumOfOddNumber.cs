f997c3d2d6abd31b5f4f27399e17b8a4bf7065bb
Marcus.Silva <marcus.silva@xpi.com.br>
2023-02-15 16:04:39 -0300

    feat(SumOfOddNumbers): add solution.

---
+++ CodeWarsChallenges/CodeWarsChallenges/Array/8kyu/SumOfOddNumber/SumOfOddNumber.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeWarsChallenges.Array._8kyu.SumOfOddNumber;
+
+public static class SumOfOddNumber
+{
+    public static long RowSumOddNumbers(long n)
+    {
+        return (long) Math.Pow(n, 3);
+    }
+}
