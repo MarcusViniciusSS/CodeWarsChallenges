f048f2271a5b57066ad441a41c67e73296357f22
Marcus.Silva <marcus.silva@xpi.com.br>
2023-02-16 14:23:19 -0300

    feat(stringrepeat): add solution.

---
+++ CodeWarsChallenges/CodeWarsChallenges/Strings/8kyu/String Repeat/StringRepeat.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+namespace CodeWarsChallenges.Strings._8kyu.String_Repeat;
+
+public static class StringRepeat
+{
+    public static string Repeat(int n, string s)
+    {
+        var stringBuilder = new StringBuilder(s.Length * n);
+
+        for (var i = 0; i < n; i++)
+            stringBuilder.Append(s);
+
+        return stringBuilder.ToString();
+    }
+}
