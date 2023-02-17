876522e700bf44de57baaa131ecb477a552a9539
Marcus.Silva <marcus.silva@xpi.com.br>
2023-02-17 11:21:12 -0300

    feat(SquareSum): add solution.

---
+++ CodeWarsChallenges/CodeWarsChallenges/Array/8kyu/Square(n)Sum/SquareSum.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeWarsChallenges.Array._8kyu.Square_n_Sum;
+
+public  static  class SquareSum
+{
+    public static int Sum(int[] numbers)
+    {
+        return numbers.Sum(item => item * item);
+    }
+}
