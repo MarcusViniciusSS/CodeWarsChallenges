7eef7f320716778180bf004851452ea5a5cb3640
Marcus.Silva <marcus.silva@xpi.com.br>
2023-02-22 15:08:22 -0300

    feat(CountingDuplicate): add solution.

---
+++ CodeWarsChallenges/CodeWarsChallenges/Strings/6kyu/Counting Duplicates/CountingDuplicate.cs
@@ -0,0 +1,29 @@
+﻿namespace CodeWarsChallenges.Strings._6kyu.Counting_Duplicates;
+
+public static class CountingDuplicate
+{
+    public static int Count(string str)
+    {
+        var lowerString = str.ToLower();
+        var hashSet = new HashSet<char>(lowerString.Length);
+        var alreadyCount = new HashSet<char>(lowerString.Length);
+        var count = 0;
+        
+        foreach (var value in lowerString)
+        {
+            if (hashSet.Contains(value))
+            {
+                if (!alreadyCount.Contains(value))
+                {
+                    count++;
+                    alreadyCount.Add(value);
+                }
+                continue;
+            }
+
+            hashSet.Add(value);
+        }
+        
+        return count;
+    }
+}
