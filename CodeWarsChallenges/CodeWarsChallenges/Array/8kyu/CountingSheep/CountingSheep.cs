d9c96846bd00a8b312025d66034d2838c7853d21
Marcus.Silva <marcus.silva@xpi.com.br>
2023-02-23 14:31:02 -0300

    feat(CountingSheep): Add solution.

---
+++ CodeWarsChallenges/CodeWarsChallenges/Array/8kyu/CountingSheep/CountingSheep.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeWarsChallenges.Array._8kyu.CountingSheep;
+
+public static class CountingSheep
+{
+    public static int Count(bool[] sheeps)
+    {
+        return sheeps.Count(sheep => sheep);
+    }
+}
