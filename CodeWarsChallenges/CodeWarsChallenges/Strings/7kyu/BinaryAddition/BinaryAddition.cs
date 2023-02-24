b4f63c6247fb99fb5036735b0f53d38978a8d05a
Marcus.Silva <marcus.silva@xpi.com.br>
2023-02-24 09:48:31 -0300

    feat(BinaryAddition): add solution.

---
+++ CodeWarsChallenges/CodeWarsChallenges/Strings/7kyu/BinaryAddition/BinaryAddition.cs
@@ -0,0 +1,17 @@
+﻿namespace CodeWarsChallenges.Strings._7kyu.BinaryAddition;
+
+public static class BinaryAddition
+{
+    public static string AddBinary(int a, int b)
+    {
+        var number = a + b;
+        var result = string.Empty;
+        for (var i = 0; number > 0; i++)
+        {
+            result = number % 2 + result;
+            number /= 2;
+        }
+    
+        return result;
+    }
+}
