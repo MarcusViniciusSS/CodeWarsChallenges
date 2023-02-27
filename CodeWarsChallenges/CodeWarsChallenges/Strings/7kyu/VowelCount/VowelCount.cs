f08ae0bd4bcddd227b074137611c5ed05a81501a
Marcus.Silva <marcus.silva@xpi.com.br>
2023-02-27 12:53:46 -0300

    feat(VowelCount): add solution.

---
+++ CodeWarsChallenges/CodeWarsChallenges/Strings/7kyu/VowelCount/VowelCount.cs
@@ -0,0 +1,14 @@
+﻿namespace CodeWarsChallenges.Strings._7kyu.VowelCount;
+
+public static class VowelCount
+{
+    private static HashSet<char> Vowels = new HashSet<char>()
+    {
+        'a', 'e', 'i', 'o', 'u'
+    };
+    
+    public static int GetVowelCount(string str)
+    {
+        return str.Count(t => Vowels.Contains(t));
+    }
+}
