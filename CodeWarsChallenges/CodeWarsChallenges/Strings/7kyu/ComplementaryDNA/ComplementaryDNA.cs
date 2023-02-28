72f7b3295c22884de2bf3d272e2b1b1b5daa35e2
Marcus.Silva <marcus.silva@xpi.com.br>
2023-02-28 13:06:59 -0300

    feat(ComplementaryDNA): add solution.

---
+++ CodeWarsChallenges/CodeWarsChallenges/Strings/7kyu/ComplementaryDNA/ComplementaryDNA.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace CodeWarsChallenges.Strings._7kyu.ComplementaryDNA;
+
+public static class ComplementaryDNA
+{
+    private static Dictionary<char, char> Deoxyribonucleic = new Dictionary<char, char>()
+    {
+        {'A', 'T'},
+        {'T', 'A'},
+        {'C', 'G'},
+        {'G', 'C'}
+    };
+    
+    public static string MakeComplement(string dna)
+    {
+       var builder = new StringBuilder(dna.Length);
+
+        foreach (var key in dna)
+        {
+            if (!Deoxyribonucleic.ContainsKey(key)) continue;
+
+            builder.Append(Deoxyribonucleic.GetValueOrDefault(key));
+        }
+        
+        return builder.ToString();
+    } 
+}
