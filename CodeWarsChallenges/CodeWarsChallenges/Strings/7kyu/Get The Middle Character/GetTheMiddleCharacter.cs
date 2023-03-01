1261d9f175bda9d8440f2bcc05ce83ad362d760a
Marcus.Silva <marcus.silva@xpi.com.br>
2023-03-01 14:23:00 -0300

    feat(GetTheMiddleCharacter): add solution.

---
+++ CodeWarsChallenges/CodeWarsChallenges/Strings/7kyu/Get The Middle Character/GetTheMiddleCharacter.cs
@@ -0,0 +1,26 @@
+﻿namespace CodeWarsChallenges.Strings._7kyu.Get_The_Middle_Character;
+
+public static class GetTheMiddleCharacter
+{
+    public static string GetMiddle(string s)
+    {
+        if (string.IsNullOrEmpty(s)) 
+            return s;
+        
+        var isEven = s.Length % 2 == 0;
+        var length = 1;
+        var index = 0;
+
+        if (s.Length != 1)
+        {
+            length = isEven ? 2 : 1;
+        }
+
+        if (length != 0)
+        {
+            index =  isEven ? (s.Length / 2) - 1 : (s.Length / 2);
+        }
+        
+        return s.Substring(index , length);
+    }
+}
