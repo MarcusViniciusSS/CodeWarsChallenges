3c0d8374d1a326458dbb4ab60326087157b49070
Marcus.Silva <marcus.silva@xpi.com.br>
2023-03-02 12:38:31 -0300

    feat(KeepHydrated): add solution.

---
+++ CodeWarsChallenges/CodeWarsChallenges/Algorithms/8kyu/Keep Hydrated/KeepHydrated.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeWarsChallenges.Algorithms._8kyu.Keep_Hydrated;
+
+public static class KeepHydrated
+{
+    public static int Litres(double time)
+    {
+        return (int)Math.Round(time * 0.5, MidpointRounding.ToZero);
+    }
+}
