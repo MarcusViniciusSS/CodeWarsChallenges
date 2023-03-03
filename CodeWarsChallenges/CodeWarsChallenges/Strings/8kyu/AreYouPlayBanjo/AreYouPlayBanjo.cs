fc4b7ac1f5227c45739ddcc94cc1f07c77f13edb
Marcus.Silva <marcus.silva@xpi.com.br>
2023-03-03 16:05:35 -0300

    feat(AreYouPlayBanjo): add solution.

---
+++ CodeWarsChallenges/CodeWarsChallenges/Strings/8kyu/AreYouPlayBanjo/AreYouPlayBanjo.cs
@@ -0,0 +1,12 @@
+﻿namespace CodeWarsChallenges.Strings._8kyu.AreYouPlayBanjo;
+
+public static class AreYouPlayBanjo
+{
+    public static string AreYouPlayingBanjo(string name)
+    {
+        if (name.StartsWith("R") || name.StartsWith("r"))
+            return $"{name} plays banjo";
+        
+        return $"{name} does not play banjo";
+    }
+}
