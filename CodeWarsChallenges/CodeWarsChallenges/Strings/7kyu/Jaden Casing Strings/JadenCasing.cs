a3f4cec7b616cb20b864105110bd6bf9f0852f66
Marcus.Silva <marcus.silva@xpi.com.br>
2023-03-07 11:42:22 -0300

    feat(JadenCasing): add solution.

---
+++ CodeWarsChallenges/CodeWarsChallenges/Strings/7kyu/Jaden Casing Strings/JadenCasing.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace CodeWarsChallenges.Strings._7kyu.Jaden_Casing_Strings;
+
+public static class JadenCasing
+{
+    public static string ToJadenCase(string phrase)
+    {
+        var words = phrase.Split(" ");
+
+        var builder = new StringBuilder(phrase.Length);
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (!char.IsLetter(words[i][0]) || char.IsUpper(words[i][0]))
+            {
+                builder.Append(words[i]);
+                
+                if ((i + 1) == words.Length) 
+                    continue;
+                
+                builder.Append(' ');
+                continue;
+            }
+            
+            builder.Append((char)(words[i][0] - 32));
+            builder.Append(words[i][1..]);
+            
+            if ((i + 1) == words.Length) 
+                continue;
+
+            builder.Append(' ');
+        }
+
+        return builder.ToString();
+    }
+}
