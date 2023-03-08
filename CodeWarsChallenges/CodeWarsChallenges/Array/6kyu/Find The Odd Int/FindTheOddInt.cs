4d77fa802d75302d97b0978fae335a6f2d26ca10
Marcus.Silva <marcus.silva@xpi.com.br>
2023-03-08 11:30:58 -0300

    feat(FindTheOddInt): add solution.

---
+++ CodeWarsChallenges/CodeWarsChallenges/Array/6kyu/Find The Odd Int/FindTheOddInt.cs
@@ -0,0 +1,13 @@
+﻿namespace CodeWarsChallenges.Array._6kyu.Find_The_Odd_Int;
+
+public static class FindTheOddInt
+{
+    public static int find_it(int[] seq)
+    {
+        if (!seq.Any()) return -1;
+        
+        var groups = seq.GroupBy(item => item);
+        
+        return groups.First(item => item.Count() % 2 != 0).Key;
+    }
+}
