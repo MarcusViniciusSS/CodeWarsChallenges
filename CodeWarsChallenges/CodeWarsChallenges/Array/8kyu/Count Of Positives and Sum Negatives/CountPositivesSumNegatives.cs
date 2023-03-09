036b9a74917ed751fdc0cb42639650ebe7bd544f
Marcus.Silva <marcus.silva@xpi.com.br>
2023-03-09 14:23:29 -0300

    feat(CountPositivesSumNegatives): add solution.

---
+++ CodeWarsChallenges/CodeWarsChallenges/Array/8kyu/Count Of Positives and Sum Negatives/CountPositivesSumNegatives.cs
@@ -0,0 +1,12 @@
+﻿namespace CodeWarsChallenges.Array._8kyu.Count_Of_Positives_and_Sum_Negatives;
+
+public static class CountPositivesSumNegatives
+{
+    public static int[] CountOfPositivesSumNegatives(int[] input)
+    {
+        if (input is null || !input.Any())
+            return new int[] { };
+        
+        return new int[2] { (int)input.LongCount(item => item > 0), input.Where(item => item < 0).Sum(item => item)  };
+    }
+}
