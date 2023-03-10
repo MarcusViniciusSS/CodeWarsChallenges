00a6f2810530399ac9552eb19230d9d0ed682ab2
Marcus.Silva <marcus.silva@xpi.com.br>
2023-03-10 14:17:11 -0300

    feat(StringEndWith): add solution.

---
+++ CodeWarsChallenges/CodeWarsChallenges/Strings/7kyu/String Ends With/StringEndWith.cs
@@ -0,0 +1,21 @@
+﻿namespace CodeWarsChallenges.Strings._7kyu.String_Ends_With;
+
+public static class StringEndWith
+{
+    public static bool EndWith(string str, string ending)
+    {
+        if (string.IsNullOrEmpty(str)) 
+            return false;
+
+        if(string.IsNullOrEmpty(ending))
+            return true;
+    
+        var startIndex = str.Length - ending.Length;
+        if (startIndex < 0)
+            return false;
+
+        var substring = str[startIndex..];
+
+        return substring == ending;
+    }
+}
