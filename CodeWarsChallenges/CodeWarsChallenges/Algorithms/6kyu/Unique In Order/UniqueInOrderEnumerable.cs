d01d11c7b39fc3303693de1779626ad595969b88
Marcus.Silva <marcus.silva@xpi.com.br>
2023-03-13 12:40:26 -0300

    feat(UniqueInOrderEnumerable): add solution.

---
+++ CodeWarsChallenges/CodeWarsChallenges/Algorithms/6kyu/Unique In Order/UniqueInOrderEnumerable.cs
@@ -0,0 +1,27 @@
+﻿namespace CodeWarsChallenges.Algorithms._6kyu.Unique_In_Order;
+
+public static class UniqueInOrderEnumerable
+{
+    public static IEnumerable<T> UniqueInOrder<T>(IEnumerable<T> iterable)
+    {
+        var array = iterable.ToArray();
+        var result = new List<T>();
+
+        if (!array.Any())
+            return Enumerable.Empty<T>();
+        
+        var preview = array[0];
+        result.Add(preview);
+        
+        for (var i = 1; i < array.Length; i++)
+        {
+            if (preview!.Equals(array[i]))
+                continue;
+
+            preview = array[i];
+            result.Add(array[i]);
+        }
+
+        return result.AsEnumerable();
+    }
+}
