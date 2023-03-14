0cbd125cf63dd678fb7f1f6b3e2b5ea2501602f7
Marcus.Silva <marcus.silva@xpi.com.br>
2023-03-14 14:33:42 -0300

    feat(SumOfTheFirstNthTermOfSeries): add solution.

---
+++ CodeWarsChallenges/CodeWarsChallenges/Algorithms/7kyu/Sum Of The First nth term of Series/SumOfTheFirstNthTermOfSeries.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace CodeWarsChallenges.Algorithms._7kyu.Sum_Of_The_First_nth_term_of_Series;
+
+public static class SumOfTheFirstNthTermOfSeries
+{
+    public static string seriesSum(int n)
+    {
+        decimal result = 0m;
+        int divisor = 1;
+    
+        while(n > 0){
+            n--;
+            result += (decimal ) 1 / divisor;
+            divisor+= 3;
+        }
+
+        return Math.Round(result, 2).ToString("0.00", CultureInfo.InvariantCulture);
+    }
+}
