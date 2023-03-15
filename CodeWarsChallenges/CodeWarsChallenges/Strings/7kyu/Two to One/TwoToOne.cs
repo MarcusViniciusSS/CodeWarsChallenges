2060acaba418de6a0ccf9b97a1cadef71ffed4cb
Marcus.Silva <marcus.silva@xpi.com.br>
2023-03-15 15:44:41 -0300

    feat(TwoToOne): add solution.

---
+++ CodeWarsChallenges/CodeWarsChallenges/Strings/7kyu/Two to One/TwoToOne.cs
@@ -0,0 +1,12 @@
+﻿namespace CodeWarsChallenges.Strings._7kyu.Two_to_One;
+
+public static class TwoToOne
+{
+    public static string Longest (string s1, string s2)
+    {
+        if (string.IsNullOrEmpty(s1) && string.IsNullOrEmpty(s2)) 
+            return string.Empty;
+
+        return new string(s1.Concat(s2).Select(item => item).Distinct().OrderBy(item => item).ToArray());
+    }
+}
