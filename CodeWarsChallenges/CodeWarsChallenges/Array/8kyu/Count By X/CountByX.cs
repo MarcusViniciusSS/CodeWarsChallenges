d2d3034b13f4a95afa54b8f30ec82d450f6accf6
Marcus.Silva <marcus.silva@xpi.com.br>
2023-03-16 15:42:19 -0300

    feat(CountByX): add solution.

---
+++ CodeWarsChallenges/CodeWarsChallenges/Array/8kyu/Count By X/CountByX.cs
@@ -0,0 +1,20 @@
+﻿namespace CodeWarsChallenges.Array._8kyu.Count_By_X;
+
+public static class CountByX
+{
+    public static int[] CountBy(int x, int n)
+    {
+        int[] z = new int[n];
+        var index = 0;
+        var value = 0;
+        while (n > 0)
+        {
+            value += x;
+            z[index] = value;
+            n--;
+            index++;
+        }
+    
+        return z;
+    }
+}
