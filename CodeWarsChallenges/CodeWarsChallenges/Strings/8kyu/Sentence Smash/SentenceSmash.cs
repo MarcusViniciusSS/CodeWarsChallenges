227fb6db8d1c8b3fbbb9dd5e304808beaed41540
Marcus.Silva <marcus.silva@xpi.com.br>
2023-03-20 12:18:03 -0300

    feat(SentenceSmash): add solution.

---
+++ CodeWarsChallenges/CodeWarsChallenges/Strings/8kyu/Sentence Smash/SentenceSmash.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace CodeWarsChallenges.Strings._8kyu.Sentence_Smash;
+
+public static class SentenceSmash
+{
+    public static string Smash(string[] words)
+    {
+        var sb = new StringBuilder();
+
+        for (var i = 0; i < words.Length; i++)
+        {
+            if (i != 0)
+            {
+                sb.Append(' ');
+            } 
+            
+            sb.Append(words[i]);
+        }
+
+        return sb.ToString();
+    }
+}
