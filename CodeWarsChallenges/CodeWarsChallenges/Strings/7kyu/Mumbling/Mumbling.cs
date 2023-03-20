8b69065111072122e5edc8b1979c912c5e86977f
Marcus.Silva <marcus.silva@xpi.com.br>
2023-03-20 12:18:42 -0300

    feat(Mumbling): add solution.

---
+++ CodeWarsChallenges/CodeWarsChallenges/Strings/7kyu/Mumbling/Mumbling.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace CodeWarsChallenges.Strings._7kyu.Mumbling;
+
+public static class Mumbling
+{
+    public static string Accum(string s) 
+    {
+        var sb = new StringBuilder();
+        var index = 0;
+        
+        foreach (var c in s)
+        {
+            sb.Append(char.ToUpper(c));
+            for (var i = 0; i < index; i++)
+            {
+                sb.Append(char.ToLower(c));
+            }
+            index++;
+            
+            if (index < s.Length)
+                sb.Append('-');
+        }
+
+        return sb.ToString();
+    }
+}
